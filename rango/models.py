e07e4f299adb1c9eeb18307fd6004409fabe3eff
Michael Hiluf <2529225h@student.gla.ac.uk>
2021-07-30 12:45:30 +0100

    Chapter 5 Excerise

--- rango/models.py
+++ rango/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     def __str__(self):
         return self.name
     
